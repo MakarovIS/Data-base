c6ff6e55e72306ac13a2099d1a82114d66a2ee70
MakarovIS <qazvo@bk.ru>
2022-12-19 22:56:44 +0300

    1

--- src/sql_base/models.py
+++ src/sql_base/models.py
@@ -1,10 +1,40 @@
 from typing import Optional
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class Workers(BaseModel):
-    id: Optional[int]
+    id: int
     surname: str
     name: str
     date_of_birth: str
-    experience: int
+    experience: Optional[str]
+
+class Spectacle(BaseModel):
+    id: int
+    name: str
+    genre: str
+    length: int
+
+class Speech(BaseModel):
+    id: int
+    spectacle_id: int
+    hall: str
+    date_and_time: datetime
+
+class Employment_of_actors(BaseModel):
+    id: int
+    worker_id: int
+    spectacle_id: int
+    role_actor: str
+
+class Ticket_price_category(BaseModel):
+    id: int
+    hall: str
+    row: str
+    price: int
+
+class Ticket(BaseModel):
+    id: int
+    ticket_price_category_id: int
+    speech_id: int
+    status: bool
